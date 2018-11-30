0a5d2538712d81e232049435a17cda1c5ebf170f
heinziheinz <beitschalbua@gmx.at>
2018-11-30 15:40:10 +0100

    starwars-plot-generator

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ErrorBoundary from './errorBoundary';
 import * as serviceWorker from './serviceWorker';
 import PlotGenerator from './js/plot-generator';
 
 
-
 ReactDOM.render(
-  <PlotGenerator />,
+  <ErrorBoundary>
+    <PlotGenerator />
+  </ErrorBoundary>,
   document.querySelector('#root')
 );
-
-/**
- * 
- */
 serviceWorker.unregister();
